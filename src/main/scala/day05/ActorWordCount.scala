a93b96919e0d895a44d34236233ad00ea442b8c8
simple <522387494@qq.com>
2018-06-06 16:47:29 +0800

    begin

---
+++ src/main/scala/day05/ActorWordCount.scala
@@ -0,0 +1,68 @@
+package day05
+
+import scala.actors.Actor
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Future
+import scala.io.Source
+
+
+/**
+  * 使用actor 并发编程实现 WordCount
+  */
+object ActorWordCount {
+  def main(args: Array[String]): Unit = {
+
+    // 文件路径
+    val files = Array("F:\\test\\scala\\words\\a.txt")
+
+    // 存放文件的结果
+    val futures = new ListBuffer[actors.Future[Any]]
+
+    // 存最后结果
+    val res: ListBuffer[Map[String, Int]] = new ListBuffer[Map[String, Int]]
+
+
+    for (file <- files) {
+      val task = new Task
+      task.start
+      val replyMsg: actors.Future[Any] = task !! SmTask(file)
+
+      futures += replyMsg // 添加到futures
+    }
+
+    while (futures.size > 0) {
+
+      // 过滤每个 Future对象， 如果None 类型的，就过滤掉
+      val dones = futures.filter(_.isSet)
+      for (done <- dones) {
+        res += done.apply.asInstanceOf[Map[String, Int]] // asInstanceOf[Map[String, Int]] 相当于java里的 (int)12L 强转
+      }
+    }
+
+    val resMap = res.flatten.groupBy(_._1).mapValues(_.foldLeft(0)(_+_._2))
+
+    println(resMap)
+
+  }
+
+
+}
+
+
+class Task extends Actor {
+  override def act(): Unit = {
+    while (true) {
+      receive {
+        case SmTask(file) => {
+          val lines = Source.fromFile(file).getLines().toList
+          val res: Map[String, Int] = lines.flatMap(_.split(" ")).map(_ -> 1).groupBy(_._1).mapValues(_.size)
+          // 异步发送
+          sender ! res
+        }
+      }
+    }
+  }
+}
+
+
+case class SmTask(file: String)
