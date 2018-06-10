d54c730ea6289a0ce71b456920dbcffdf4372c53
simple <522387494@qq.com>
2018-06-11 00:06:09 +0800

    EXERCISE SPARK RDD

--- src/main/scala/Day08/SparkRDDExercise.scala
+++ src/main/scala/Day08/SparkRDDExercise.scala
@@ -98,16 +98,16 @@ object SparkRDDExercise {
 
     // TODO 按value的降序排序
     // TODO 第一种 sortBy(第一个参数表示 按照元组里的什么进行排序 第二个表示是否是 升序 true 是升序 false 是降序)
-    //    val res = rdd11.reduceByKey(_+_).sortBy(_._2,false)
+//        val res = rdd11.reduceByKey(_+_).sortBy(_._2,false)
     // TODO 第二种 两极反转 7777777777777
     //    val res = rdd11.reduceByKey(_+_).map(e =>(e._2,e._1)).sortByKey(false).map(e=>(e._1,e._2))
 
     // TODO 笛卡尔积
     // 结果 ArrayBuffer(((tom,1),(jerry,2)), ((tom,1),(tom,3)), ((tom,1),(shuke,2)), ((tom,1),(kitty,5)), ((jerry,3),(jerry,2)), ((jerry,3),(tom,3)), ((jerry,3),(shuke,2)), ((jerry,3),(kitty,5)), ((kitty,2),(jerry,2)), ((kitty,2),(tom,3)), ((kitty,2),(shuke,2)), ((kitty,2),(kitty,5)), ((shuke,1),(jerry,2)), ((shuke,1),(tom,3)), ((shuke,1),(shuke,2)), ((shuke,1),(kitty,5)))
-    val res = rdd11.cartesian(rdd12)
+//    val res = rdd11.cartesian(rdd12)
 
     // 其他 count top take first takeOrdered
-    println(res.collect.toBuffer)
+//    println(res.collect.toBuffer)
   }
 }
 
@@ -115,6 +115,97 @@ object SparkRDDExercise {
 object Exercise {
 
   def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("SparkRDDExercise").setMaster("local")
+    val sc = new SparkContext(conf)
 
+
+    // TODO 通过并行化生成 rdd
+    val rdd1 = sc.parallelize(List(1, 5, 4, 8, 6, 2, 5, 565, 56, 78, 110, 10))
+
+
+    // TODO 对rdd1里的每个元素乘以2然后排序
+    // 第一个是 输出什么 第二个是升序 还是倒叙  true 是升序  false 是倒叙
+
+
+
+
+    // TODO 过滤出大于等于10的元素
+
+
+    // TODO 将元素以数组的方式打印出来
+
+
+    val rdd2 = sc.parallelize(Array("a b c", "e f s", "h i d"))
+    // TODO 将rdd2里面的每个元素先切分在压平
+
+
+    val rdd3 = sc.parallelize(List(List("A D G", "D F G"), List("Q I O", "P L N"), List("Z C B", "M D T")))
+    // TODO 将rdd3里的每个元素先切分在压平
+
+
+    val rdd4 = sc.parallelize(List(5, 9, 6, 3, 3))
+    val rdd5 = sc.parallelize(List(45, 9, 3, 15, 5))
+    // TODO 求并集
+
+
+
+    // TODO 求交集
+
+
+    // TODO 去重
+
+
+
+    // 单词 ->个数
+    val rdd6 = sc.parallelize(List(("tom", 1), ("jerry", 3), ("kitty", 2)))
+    val rdd7 = sc.parallelize(List(("lijun", 5), ("tom", 99), ("kitty", 2)))
+
+    // TODO 求join
+    // 在类型（K，V）和（K，W）的数据集上调用时，返回包含每个键的所有元素对的（K，（V，W））对的数据集。
+    // 就比如 rdd6 里面的 tom -> 1 和 tom ->99 会形成 tom ->(1,99)
+
+
+    // TODO 求左连接和右连接  和数据库的一个意思 笛卡尔积
+
+
+
+    // TODO 求并集
+
+
+
+    // TODO 按key进行分组
+
+
+    // TODO 分别用groupByKey 和 reduceByKey 实现单词计数 注意groupByKey与reduceByKey的区别
+
+
+
+    // 单词 ->个数
+    val rdd8 = sc.parallelize(List(("tom", 1), ("tom", 2), ("jerry", 3), ("kitty", 2)))
+    val rdd9 = sc.parallelize(List(("jerry", 2), ("tom", 99), ("shuke", 2)))
+
+    // TODO cproup 注意cogroup和groupByKey 的区别
+
+
+    val rdd10 = sc.parallelize(List(1, 2, 3, 4, 5))
+    // TODO reduce 聚合
+
+
+    val rdd11 = sc.parallelize(List(("tom", 1), ("jerry", 3), ("kitty", 2), ("shuke", 1)))
+    val rdd12 = sc.parallelize(List(("jerry", 2), ("tom", 3), ("shuke", 2), ("kitty", 5)))
+
+    val rdd13 = rdd11.union(rdd12)
+
+    // TODO 按value的降序排序
+    // TODO 第一种 sortBy(第一个参数表示 按照元组里的什么进行排序 第二个表示是否是 升序 true 是升序 false 是降序)
+
+    // TODO 第二种 两极反转 7777777777777
+
+
+    // TODO 笛卡尔积
+
+    // 其他 count top take first takeOrdered
+
+//    println(res.collect.toBuffer)
   }
 }
---
+++ src/main/scala/day09/WebNetCount.scala
@@ -0,0 +1,103 @@
+package day09
+
+import java.net.URL
+import java.util.UUID
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{HashPartitioner, Partitioner, SparkConf, SparkContext}
+
+import scala.collection.mutable
+
+object WebNetCount {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("SparkRDDExercise").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    // 获取数据
+    val file: RDD[String] = sc.textFile("F:\\test\\spark\\input")
+
+    // 取出 url 并生成一个元组
+
+    val urlAndOne: RDD[(String, Int)] = file.filter(_.split(" ").length == 2) map (line => {
+      val fields = line.split(" ")
+      val url = fields(1)
+      (url, 1)
+    })
+
+    // 把相同的url聚合
+    val sumedUrl: RDD[(String, Int)] = urlAndOne.reduceByKey(_ + _)
+
+    // 获取学科信息并缓存  因为这边的数据以后可能还会用到
+    val cachedProject: RDD[(String, (String, Int))] = sumedUrl.map(x => {
+      val url = x._1
+      val project = new URL(url).getHost
+      val count = x._2
+      (url, (project, count))
+    }).cache()
+
+
+    // 调用Spark自带的分区器此时会发生哈希碰撞，需要自定义分区器
+    //    val res = cachedProject.partitionBy(new HashPartitioner(3))
+    //    res.saveAsTextFile("")
+
+
+    // 得到所有学科
+    val porjects = cachedProject.keys.distinct.collect
+    println(porjects.toBuffer)
+    // 调用自定义分区器 并得到分区号
+    val partitioner = new WebNetPartitioner(porjects)
+
+    // 分区
+    val partitioned = cachedProject.partitionBy(partitioner)
+
+    // 对每个分区的数据进行排序并去top3
+    val res = partitioned.mapPartitions(it => {
+      it.toList.sortBy(_._2._2).reverse.iterator
+    })
+
+    res.saveAsTextFile("F:\\test\\spark\\out\\" + UUID.randomUUID())
+
+    sc.stop
+  }
+}
+
+
+class WebNetPartitioner(projects: Array[String]) extends Partitioner {
+
+
+  // 用来存放科学和分区号
+  private val projectsAndPartNum = new mutable.HashMap[String, Int]
+
+  // 计数器 用于指定分区号
+  var n = 0
+
+  for (pro <- projects) {
+    projectsAndPartNum += (pro -> n)
+    n += 1
+  }
+
+  // 得到分区数
+  override def numPartitions: Int = projects.length
+
+  // 得到分区号
+  override def getPartition(key: Any): Int = {
+    projectsAndPartNum.getOrElse(key.toString, 0)
+  }
+
+
+}
+
+object TTT {
+
+  def main(args: Array[String]): Unit = {
+    val file = Array("票 2", "票 2", "票 2", "票 2", "票 2", "票 2", "票 2", "票 2", "票 2")
+    val res = file.filter(_.split(" ").length == 2) map (line => {
+      val fields = line.split(" ")
+      val url = fields(1)
+      (url, 1)
+    })
+
+    println(res.toBuffer)
+  }
+}
